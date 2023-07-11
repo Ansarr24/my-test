1a469b4d5f3760d38270bc4c09f142d11c71b97e
Ansarr24 <ansarr97@gmail.com>
2023-07-11 17:21:30 +0800

    Commit message

--- src/component/AddContact.js
+++ src/component/AddContact.js
@@ -22,14 +22,14 @@ class AddContact extends React.Component{
                 <h2>Add Contact</h2>
                 <form className="ui form" onSubmit ={this.add}>
                     <div className="field">
-                        <label>Name</label>
+                        <label>Name:</label>
                         <input type="text" placeholder="Name"
                         value={this.state.name}
                         onChange={ (e) => this.setState({name: e.target.value})}/>
                     </div>
 
                     <div className="field">
-                        <label>Email</label>
+                        <label>Email:</label>
                         <input type="text" placeholder="Email"
                         value={this.state.email}
                         onChange={ (e) => this.setState({email: e.target.value})}/>
